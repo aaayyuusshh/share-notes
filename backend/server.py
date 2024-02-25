22254d420b8f87700158fe4b89292bfe1e6aaa13
Raunak Sandhu <raunaksandhu@Raunaks-MacBook-Pro.local>
2024-02-24 18:00:58 -0700

    test

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from typing import Annotated
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
 
 from db import (
     UserCreate,
@@ -32,3 +33,56 @@ async def get_users(s: Session) -> list[User]:
 @app.post("/users/")
 async def post_user(s: Session, uc: UserCreate) -> User:
     return await create_user(s, uc)
+
+
+
+# Raunak added code
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
