6a3138cf4176ce491cc8f696124c6d5d416e8246
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-02-24 21:17:41 -0700

    Handle text chunks + beautify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,43 @@
 import './App.css'
-import {useState, React} from 'react'
+import {React, useState, useEffect} from 'react'
 
 export default function App() {
-
   const [textValue, setTextValue] = useState("");
+  const [chunks, setChunks] = useState([]);
+
+  useEffect(() => {
+    handleChunks();
+  },[textValue]);
 
   function handleUpdate(event) {
-      setTextValue(event.target.value)
+      setTextValue(event.target.value);
+  }
+
+  //handle document text data, chunk by chunk
+  function handleChunks() {
+    const chunkSize = 3;
+    const chunksArray = [];
+    for(let i=0; i<textValue.length; i+=chunkSize) {
+      chunksArray.push(textValue.substring(i, i+chunkSize));
+    }
+    setChunks(chunksArray);
+    console.log(chunksArray)
   }
 
   return (
-    <div class="container mainContainer">
-      <nav class="navBar">
-        <h1>📕 share-note.</h1>
+    <div className="mainContainer">
+      <nav className="navBar">
+        <h2 className="logoText">📕 share-note.</h2>
       </nav>
-      <div>
-        <label htmlFor="textArea">Type Below</label>
-        <textarea 
-          className="textArea" name="postContent" id="textArea" cols="150" rows="32" placeholder="start your document...."
-          value={textValue} 
-          onChange={handleUpdate}>
-        </textarea>
+      <div className="textContainer">
+        <div className="container">
+          <label className="textLabel" htmlFor="textArea">Document 1</label>
+            <textarea 
+              className="textArea" name="postContent" id="textArea" cols="90" rows="32" placeholder="start your document...."
+              value={textValue} 
+              onChange={handleUpdate}>
+            </textarea>
+        </div>
       </div>
     </div>
   )
