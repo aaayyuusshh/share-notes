28bfc20d80479b3997c830b13d763df3015cbd87
Raunak Sandhu <raunaksandhu10@gmail.com>
2024-03-14 14:44:17 -0600

    fix double requests

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,5 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
