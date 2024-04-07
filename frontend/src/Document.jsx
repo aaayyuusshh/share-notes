726c371991c1c9ddb353d8e1963b8b2fd4db3b2f
Aashirbadd <60020431+Aashirbadd@users.noreply.github.com>
2024-04-06 20:48:51 -0600

    Format all front end files with prettier

--- frontend/src/Document.jsx
+++ frontend/src/Document.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 export default function Document() {
   const [textValue, setTextValue] = useState("");
 
-  const MASTER_IP = "localhost"
+  const MASTER_IP = "localhost";
 
-  const { ip, port, id, docName } = useParams()
+  const { ip, port, id, docName } = useParams();
 
   const [webSocket, setWebSocket] = useState(null);
 
@@ -29,15 +29,39 @@ export default function Document() {
     // Sending 'canEdit' will be false in everycase except when the clinet was editing and lost connection
     // In that case, the canEdit flag is used to tell the replica to generate a new token and make sure this client gets
     // first access to continue editing seamlessly
-    console.log('connect to websocket: ' + ip + ':' + port + '/ws/' + id + '/' + docName + '/' + CAN_EDIT + '/');
-    const ws = new WebSocket('ws://' + ip + ':' + port + '/ws/' + id + '/' + docName + '/' + CAN_EDIT + '/');
+    console.log(
+      "connect to websocket: " +
+        ip +
+        ":" +
+        port +
+        "/ws/" +
+        id +
+        "/" +
+        docName +
+        "/" +
+        CAN_EDIT +
+        "/"
+    );
+    const ws = new WebSocket(
+      "ws://" +
+        ip +
+        ":" +
+        port +
+        "/ws/" +
+        id +
+        "/" +
+        docName +
+        "/" +
+        CAN_EDIT +
+        "/"
+    );
 
     ws.onopen = () => {
-      console.log('WebSocket Connected');
+      console.log("WebSocket Connected");
     };
     ws.onmessage = (event) => {
-      console.log('Message from server ', event.data);
-      console.log('IN THE connectWebSocket function');
+      console.log("Message from server ", event.data);
+      console.log("IN THE connectWebSocket function");
       setIsReconnecting(false);
       setIsLoading(false);
 
@@ -46,20 +70,20 @@ export default function Document() {
         console.log("canEdit textbox " + canEdit);
         CAN_EDIT = true;
         console.log("CAN_EDIT " + CAN_EDIT);
-      }
-      else if (!CAN_EDIT) {    // FIX SOON
-        setTextValue(event.data)
+      } else if (!CAN_EDIT) {
+        // FIX SOON
+        setTextValue(event.data);
       }
       // Shouldn't need this as only once client can update
       //setTextValue(event.data);
     };
 
     ws.onclose = () => {
-      console.log('WebSocket disconnected');
+      console.log("WebSocket disconnected");
       setIsLoading(true);
       // canEdit can't be used as I pass it as a flag to the websocket connection
       setIsReconnecting(true); // used to gray out the textbox
-      
+
       requestNewIPAndPort(ip, port);
     };
 
@@ -70,36 +94,36 @@ export default function Document() {
     const bodyObj = JSON.stringify({
       IP: ip,
       PORT: port,
-      docID: id
-    })
-    console.log(bodyObj)
+      docID: id,
+    });
+    console.log(bodyObj);
     try {
-      fetch('http://'+ MASTER_IP +':8000/lostConnection/', {
+      fetch("http://" + MASTER_IP + ":8000/lostConnection/", {
         method: "POST",
         header: {
-          'Accept': 'application/json',
+          Accept: "application/json",
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
           IP: ip,
           PORT: port,
           docID: id,
-        })
+        }),
       })
-      .then((response) => response.json())
-      .then((data) => {
-      const IP = data.IP.toString()
-      const PORT = data.port.toString()
-      console.log(IP)
-      console.log(PORT)
-      if (IP && PORT) {
-        reconnectWebSocket(IP, PORT);
-      } else {
-        console.error('Failed to get new IP and port');
-      }
-    })
+        .then((response) => response.json())
+        .then((data) => {
+          const IP = data.IP.toString();
+          const PORT = data.port.toString();
+          console.log(IP);
+          console.log(PORT);
+          if (IP && PORT) {
+            reconnectWebSocket(IP, PORT);
+          } else {
+            console.error("Failed to get new IP and port");
+          }
+        });
     } catch (error) {
-      console.error('Error fetching new IP and port:', error);
+      console.error("Error fetching new IP and port:", error);
     }
   };
 
@@ -112,17 +136,16 @@ export default function Document() {
     connectWebSocket(IP, PORT);
   };
 
-
   const handleStartEditing = () => {
     setIsLoading(true);
     //NOTE: On the server side, any message sent is interpreted as a request to edit
-    webSocket.send(JSON.stringify({ startEdit: true}));
+    webSocket.send(JSON.stringify({ startEdit: true }));
   };
 
   const handleStopEditing = () => {
     setCanEdit(false);
     CAN_EDIT = false;
-    webSocket.send(JSON.stringify({ content: "*** STOP EDITING ***"}));
+    webSocket.send(JSON.stringify({ content: "*** STOP EDITING ***" }));
   };
 
   function handleUpdate(event) {
@@ -135,43 +158,88 @@ export default function Document() {
     }
   }
 
-  function navigateHome(){
+  function navigateHome() {
     navigate(`/`);
   }
 
   return (
     <div>
-      <nav style={{ backgroundColor: '#333', color: '#fff', padding: '10px', display:'flex' }}>
-        <button 
-        onClick={navigateHome}
-        style={{backgroundColor:'#333', border:'none',  marginRight:'25px' }}
+      <nav
+        style={{
+          backgroundColor: "#333",
+          color: "#fff",
+          padding: "10px",
+          display: "flex",
+        }}
+      >
+        <button
+          onClick={navigateHome}
+          style={{
+            backgroundColor: "#333",
+            border: "none",
+            marginRight: "25px",
+          }}
         >
-          <img 
-            src={'/imgs/homeButton.png'}
-            alt="Home" 
-            style={{width:'30px'}}
-            />
+          <img
+            src={"/imgs/homeButton.png"}
+            alt="Home"
+            style={{ width: "30px" }}
+          />
         </button>
-        <h2 style={{ margin: '2px' }}>{docName}</h2>
+        <h2 style={{ margin: "2px" }}>{docName}</h2>
       </nav>
       <div>
-        <div style={{ border: 'none', padding: '15px' }}>
+        <div style={{ border: "none", padding: "15px" }}>
           {!canEdit && (
             <>
               {isLoading ? (
-                <div style={{ fontStyle: 'italic', color: '#777' }}>Loading...</div>
+                <div style={{ fontStyle: "italic", color: "#777" }}>
+                  Loading...
+                </div>
               ) : (
-                <button style={{ backgroundColor: '#4285F4', color: '#fff', border: 'none', padding: '10px 20px', cursor: 'pointer', borderRadius:'20px' }} onClick={handleStartEditing}>Start Editing</button>
+                <button
+                  style={{
+                    backgroundColor: "#4CAF50",
+                    color: "#fff",
+                    border: "none",
+                    padding: "10px 20px",
+                    cursor: "pointer",
+                    borderRadius: "20px",
+                  }}
+                  onClick={handleStartEditing}
+                >
+                  Start Editing
+                </button>
               )}
             </>
           )}
           {canEdit && (
             <>
-              <button style={{ backgroundColor: '#ff0040', color: '#fff', border: 'none', padding: '10px 20px', cursor: 'pointer', borderRadius:'20px'  }} onClick={handleStopEditing}>Stop Editing</button>
+              <button
+                style={{
+                  backgroundColor: "#ff0040",
+                  color: "#fff",
+                  border: "none",
+                  padding: "10px 20px",
+                  cursor: "pointer",
+                  borderRadius: "20px",
+                }}
+                onClick={handleStopEditing}
+              >
+                Stop Editing
+              </button>
             </>
           )}
           <textarea
-            style={{ height:'100%', width: '100%', padding: '10px', marginTop: '10px', border: '1px solid #ccc', borderRadius: '5px', resize: 'none' }}
+            style={{
+              height: "100%",
+              width: "100%",
+              padding: "10px",
+              marginTop: "10px",
+              border: "1px solid #ccc",
+              borderRadius: "5px",
+              resize: "none",
+            }}
             id="textArea"
             cols="410"
             resize
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
   const [docList, setDocList] = useState([]);
   const [idSelected, setIdSelected] = useState("");
   const [nameSelected, setNameSelected] = useState("");
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const MASTER_IP = "localhost"
+  const MASTER_IP = "localhost";
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
@@ -23,154 +23,177 @@ const Home = () => {
   let navigate = useNavigate();
 
   const navigateToNewDocument = () => {
-    console.log(docName)
-    fetch('http://' + MASTER_IP + ':8000/createDocAndConnect/', {
+    console.log(docName);
+    fetch("http://" + MASTER_IP + ":8000/createDocAndConnect/", {
       method: "POST",
       header: {
         "Content-Type": "application/json",
       },
       body: docName,
     })
-    .then((response) => response.json())
-    .then((data) => {
-      const docID = data.docID.toString()
-      const docName = data.docName.toString()
-      const IP = data.IP.toString()
-      const port = data.port.toString()
-      console.log(docID)
-      console.log(docName)
-      console.log(IP)
-      console.log(port)
-      navigate(`/document/` + IP + '/' + port + '/' + docID + '/' + docName);
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        const docID = data.docID.toString();
+        const docName = data.docName.toString();
+        const IP = data.IP.toString();
+        const port = data.port.toString();
+        console.log(docID);
+        console.log(docName);
+        console.log(IP);
+        console.log(port);
+        navigate(`/document/` + IP + "/" + port + "/" + docID + "/" + docName);
+      });
     //return // Commented out
   };
 
   const navigateToExistingDocument = () => {
-    console.log(idSelected)
-    console.log(nameSelected)
-    fetch('http://' + MASTER_IP + ':8000/connectToExistingDoc/', {
+    console.log(idSelected);
+    console.log(nameSelected);
+    fetch("http://" + MASTER_IP + ":8000/connectToExistingDoc/", {
       method: "POST",
       header: {
         "Content-Type": "application/json",
       },
       body: idSelected,
     })
-    .then((response) => response.json())
-    .then((data) => {
-      const IP = data.IP.toString()
-      const port = data.port.toString()
-      console.log(IP)
-      console.log(port)
-      // TODO: Pass these variabes as states (or something else, I don't really know JS...) include IP in those variables
-      // currently this only connects to 127.0.0.1 (i.e. localhost) 
-      navigate(`/document/` + IP + '/' + port + '/' + idSelected + '/' + nameSelected);
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        const IP = data.IP.toString();
+        const port = data.port.toString();
+        console.log(IP);
+        console.log(port);
+        // TODO: Pass these variabes as states (or something else, I don't really know JS...) include IP in those variables
+        // currently this only connects to 127.0.0.1 (i.e. localhost)
+        navigate(
+          `/document/` + IP + "/" + port + "/" + idSelected + "/" + nameSelected
+        );
+      });
   };
 
   // Get Document List from the server
   const getDocList = async () => {
-    const response = await fetch('http://'+ MASTER_IP +':8000/docList/')
-    setDocList(Object.entries(await response.json()))
-    console.log(response)
-  }
+    const response = await fetch("http://" + MASTER_IP + ":8000/docList/");
+    setDocList(Object.entries(await response.json()));
+    console.log(response);
+  };
 
   useEffect(() => {
-    getDocList()
+    getDocList();
   }, []);
 
   return (
     <>
-    <div className="homePage" style={{ textAlign: 'center', padding: '20px', backgroundColor: '#f0f0f0' }}>
-      <h1 style={{ color: '#333' }}>Welcome to Share Notes</h1>
-      <input
-        type="text"
-        placeholder="Provide a new document name..."
-        value={docName}
-        onChange={handleChange}
-        style={{ padding: '8px', margin: '10px', width: '60%', borderRadius: '4px', border: '1px solid #ccc' }}
-      />
-      <button
-        disabled={!docName}
-        onClick={navigateToNewDocument}
-        style={{
-          padding: '8px 15px',
-          backgroundColor: '#4CAF50',
-          color: 'white',
-          border: 'none',
-          borderRadius: '20px',
-          cursor: 'pointer',
-        }}
-      >
-        Create New Doc
-      </button>
-    </div>
-    <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <h2 style={{ color: '#333' }}>Document List</h2>
-      <input
-        type="text"
-        placeholder="Search by document name..."
-        value={searchTerm}
-        onChange={handleSearchChange}
-        style={{
-          padding: '8px',
-          margin: '10px',
-          width: '60%',
-          borderRadius: '4px',
-          border: '1px solid #ccc',
-        }}
-      />
       <div
+        className="homePage"
         style={{
-          height: '40vh',
-          overflowY: 'auto',
+          textAlign: "center",
+          padding: "20px",
+          backgroundColor: "#f0f0f0",
         }}
       >
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          {filteredDocList.map(([index, doc], i) => (
-            <button
-              key={index}
-              onClick={() => {
-                setIdSelected(doc.id);
-                setNameSelected(doc.name);
-              }}
-              style={{
-                width: '60%',
-                padding: '15px',
-                backgroundColor: i % 2 === 0 ? 'white' : '#f0f0f0',
-                border: 'none',
-                borderRadius: '8px',
-                marginBottom: '5px',
-                cursor: 'pointer',
-                outline: 'none',
-                transition: 'background-color 0.3s ease-in-out',
-                ...(idSelected === doc.id && { backgroundColor: '#4285F4', color: 'white' }),
-              }}
-            >
-              {`${doc.id}: ${doc.name}`}
-            </button>
-          ))}
-        </div>
+        <h1 style={{ color: "#333" }}>Welcome to Share Notes</h1>
+        <input
+          type="text"
+          placeholder="Provide a new document name..."
+          value={docName}
+          onChange={handleChange}
+          style={{
+            padding: "8px",
+            margin: "10px",
+            width: "60%",
+            borderRadius: "4px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <button
+          disabled={!docName}
+          onClick={navigateToNewDocument}
+          style={{
+            padding: "8px 15px",
+            backgroundColor: "#4CAF50",
+            color: "white",
+            border: "none",
+            borderRadius: "20px",
+            cursor: "pointer",
+          }}
+        >
+          Create New Doc
+        </button>
       </div>
-      <button
-        disabled={!idSelected}
-        onClick={navigateToExistingDocument}
-        style={{
-          padding: '8px 15px',
-          marginTop: '20px',
-          backgroundColor: '#4285F4',
-          color: 'white',
-          border: 'none',
-          borderRadius: '20px',
-          cursor: 'pointer',
-        }}
-      >
-        Open Selected Document
-      </button>
-    </div>
-  </>
-  );  
-  
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <h2 style={{ color: "#333" }}>Document List</h2>
+        <input
+          type="text"
+          placeholder="Search by document name..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+          style={{
+            padding: "8px",
+            margin: "10px",
+            width: "60%",
+            borderRadius: "4px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <div
+          style={{
+            height: "40vh",
+            overflowY: "auto",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            {filteredDocList.map(([index, doc], i) => (
+              <button
+                key={index}
+                onClick={() => {
+                  setIdSelected(doc.id);
+                  setNameSelected(doc.name);
+                }}
+                style={{
+                  width: "60%",
+                  padding: "15px",
+                  backgroundColor: i % 2 === 0 ? "white" : "#f0f0f0",
+                  border: "none",
+                  borderRadius: "8px",
+                  marginBottom: "5px",
+                  cursor: "pointer",
+                  outline: "none",
+                  transition: "background-color 0.3s ease-in-out",
+                  ...(idSelected === doc.id && {
+                    backgroundColor: "#4285F4",
+                    color: "white",
+                  }),
+                }}
+              >
+                {`${doc.id}: ${doc.name}`}
+              </button>
+            ))}
+          </div>
+        </div>
+        <button
+          disabled={!idSelected}
+          onClick={navigateToExistingDocument}
+          style={{
+            padding: "8px 15px",
+            marginTop: "20px",
+            backgroundColor: "#4285F4",
+            color: "white",
+            border: "none",
+            borderRadius: "20px",
+            cursor: "pointer",
+          }}
+        >
+          Open Selected Document
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Home;
