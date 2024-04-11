3b874eb137c341692048aa45ae8302ac9ee8183e
raunak8016 <raunaksandhu10@gmail.com>
2024-04-11 13:55:52 -0600

    update

--- backend/replica/start_replica_network.sh
+++ backend/replica/start_replica_network.sh
@@ -3,9 +3,9 @@
 echo "PORT: $1"
 export PORT=$1;
 # enter the IP address of network connection for the replica
-export IP="10.13.67.149";
+export IP="10.13.63.43";
 # enter the IP address of network connection for the master server
-export MASTER_IP="10.13.67.149";
+export MASTER_IP="10.13.63.43";
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
